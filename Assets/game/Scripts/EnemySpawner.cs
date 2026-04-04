3a2c5f4d270db442af353263361412bcf12eeb68
Edson Rodriguez <edson.rod.p19@gmail.com>
2026-04-04 14:52:31 -0700

    feat: add repeating spawn timer to EnemySpawner

--- Assets/game/Scripts/EnemySpawner.cs
+++ Assets/game/Scripts/EnemySpawner.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnInterval = 5f;
+    private float spawnTimer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spawnTimer = spawnInterval;
+
         if (enemyPrefab == null)
         {
             Debug.LogError("Enemy prefab is not assigned in the inspector.");
@@ -19,7 +24,12 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            SpawnEnemy();
+            spawnTimer = spawnInterval;
+        }
     }
 
     void SpawnEnemy()
