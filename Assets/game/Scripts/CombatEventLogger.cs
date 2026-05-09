35278c9839c8f4efd0570c9b683575ab552c5e46
Edson Rodriguez <edson.rod.p19@gmail.com>
2026-05-09 16:05:44 -0700

    feat: add combat event logger subscriber

---
+++ Assets/game/Scripts/CombatEventLogger.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CombatEventLogger : MonoBehaviour
+{
+    void OnEnable()
+    {
+        CombatEvents.OnAttack += HandleAttack;
+        CombatEvents.OnHit += HandleHit;
+        CombatEvents.OnDeath += HandleDeath;
+    }
+
+    void OnDisable()
+    {
+        CombatEvents.OnAttack -= HandleAttack;
+        CombatEvents.OnHit -= HandleHit;
+        CombatEvents.OnDeath -= HandleDeath;
+    }
+
+    void HandleAttack(Transform attacker, Transform target)
+    {
+        Debug.Log("CombatEventLogger: " + attacker.name + " attacked " + target.name);
+    }
+
+    void HandleHit(Transform attacker, Transform target, float damage)
+    {
+        Debug.Log("CombatEventLogger: " + attacker.name + " hit " + target.name + " for " + damage + " damage");
+    }
+
+    void HandleDeath(Transform character)
+    {
+        Debug.Log("CombatEventLogger: " + character.name + " has died");
+    }
+}
--- Assets/game/Scripts/CombatEvents.cs
+++ Assets/game/Scripts/CombatEvents.cs
@@ -16,22 +16,18 @@ public static class CombatEvents
     // Call this method to trigger the OnAttack event
     public static void TriggerAttack(Transform attacker, Transform target)
     {
-        Debug.Log("Attack event triggered: " + attacker.name + " -> " + target.name);
-        OnAttack?.Invoke(attacker, target);
-        
+        OnAttack?.Invoke(attacker, target);   
     }
 
     // Call this method to trigger the OnHit event
     public static void TriggerHit(Transform attacker, Transform target, float damage)
     {
-        Debug.Log("Hit event triggered: " + attacker.name + " hit " + target.name + " for " + damage);
         OnHit?.Invoke(attacker, target, damage);
     }
 
     // Call this method to trigger the OnDeath event
     public static void TriggerDeath(Transform character)
     {
-        Debug.Log("Death event triggered: " + character.name);
         OnDeath?.Invoke(character);
     }
 
